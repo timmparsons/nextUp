3977c5e16a2727c01233cc972e1369ee4adc1ee8
Tim Parsons <timmparsons85@gmail.com>
2023-05-22 21:48:03 -0700

    added user siice

--- store/store.js
+++ store/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import movieReducer from '../redux/slices/movieSlice';
+import userSlice from '../redux/slices/movieSlice';
 
 export const store = configureStore({
   reducer: {
-		movie: movieReducer
-	},
-})
+    movie: movieReducer,
+    user: userSlice
+  }
+});
