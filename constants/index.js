9a7b725fa6f7420f1b9003b36206b352b71b7c19
Tim Parsons <timmparsons85@gmail.com>
2023-05-26 07:08:52 -0700

    added scrollbar and tidied up api calls

--- constants/index.js
+++ constants/index.js
@@ -1,217 +1,198 @@
 export const MOVIE_LIST = [
   {
-    "adult": false,
-    "backdrop_path": "/22z44LPkMyf5nyyXvv8qQLsbom.jpg",
-    "genre_ids": [
-        27,
-        9648,
-        53
-    ],
-    "id": 631842,
-    "original_language": "en",
-    "original_title": "Knock at the Cabin",
-    "overview": "While vacationing at a remote cabin, a young girl and her two fathers are taken hostage by four armed strangers who demand that the family make an unthinkable choice to avert the apocalypse. With limited access to the outside world, the family must decide what they believe before all is lost.",
-    "popularity": 3012.719,
-    "poster_path": "/dm06L9pxDOL9jNSK4Cb6y139rrG.jpg",
-    "release_date": "2023-02-01",
-    "title": "Knock at the Cabin",
-    "video": false,
-    "vote_average": 6.5,
-    "vote_count": 907
-	},
-	{
-		"adult": false,
-		"backdrop_path": "/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg",
-		"genre_ids": [
-				28,
-				12,
-				878
-		],
-		"id": 505642,
-		"original_language": "en",
-		"original_title": "Black Panther: Wakanda Forever",
-		"overview": "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death.  As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
-		"popularity": 2325.121,
-		"poster_path": "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
-		"release_date": "2022-11-09",
-		"title": "Black Panther: Wakanda Forever",
-		"video": false,
-		"vote_average": 7.3,
-		"vote_count": 3945
-	},
-	{
-    "adult": false,
-    "backdrop_path": "/nDxJJyA5giRhXx96q1sWbOUjMBI.jpg",
-    "genre_ids": [
-        28,
-        35,
-        14,
-        12
-    ],
-    "id": 594767,
-    "original_language": "en",
-    "original_title": "Shazam! Fury of the Gods",
-    "overview": "Billy Batson and his foster siblings, who transform into superheroes by saying \"Shazam!\", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.",
-    "popularity": 5286.148,
-    "poster_path": "/2VK4d3mqqTc7LVZLnLPeRiPaJ71.jpg",
-    "release_date": "2023-03-15",
-    "title": "Shazam! Fury of the Gods",
-    "video": false,
-    "vote_average": 6.8,
-    "vote_count": 1380
-},
-{
-	"adult": false,
-	"backdrop_path": "/bT3IpP7OopgiVuy6HCPOWLuaFAd.jpg",
-	"genre_ids": [
-			35,
-			9648,
-			28
-	],
-	"id": 638974,
-	"original_language": "en",
-	"original_title": "Murder Mystery 2",
-	"overview": "After starting their own detective agency, Nick and Audrey Spitz land a career-making case when their billionaire pal is kidnapped from his wedding.",
-	"popularity": 2099.981,
-	"poster_path": "/s1VzVhXlqsevi8zeCMG9A16nEUf.jpg",
-	"release_date": "2023-03-28",
-	"title": "Murder Mystery 2",
-	"video": false,
-	"vote_average": 6.6,
-	"vote_count": 920
-},
-{
-	"adult": false,
-	"backdrop_path": "/44immBwzhDVyjn87b3x3l9mlhAD.jpg",
-	"genre_ids": [
-			27,
-			9648,
-			53
-	],
-	"id": 934433,
-	"original_language": "en",
-	"original_title": "Scream VI",
-	"overview": "Following the latest Ghostface killings, the four survivors leave Woodsboro behind and start a fresh chapter.",
-	"popularity": 3854.657,
-	"poster_path": "/wDWwtvkRRlgTiUr6TyLSMX8FCuZ.jpg",
-	"release_date": "2023-03-08",
-	"title": "Scream VI",
-	"video": false,
-	"vote_average": 7.4,
-	"vote_count": 908
-},
-{
-	"adult": false,
-	"backdrop_path": "/b9UCfDzwiWw7mIFsIQR9ZJUeh7q.jpg",
-	"genre_ids": [
-			10749,
-			28,
-			35
-	],
-	"id": 868759,
-	"original_language": "en",
-	"original_title": "Ghosted",
-	"overview": "Salt-of-the-earth Cole falls head over heels for enigmatic Sadie — but then makes the shocking discovery that she’s a secret agent. Before they can decide on a second date, Cole and Sadie are swept away on an international adventure to save the world.",
-	"popularity": 4667.034,
-	"poster_path": "/liLN69YgoovHVgmlHJ876PKi5Yi.jpg",
-	"release_date": "2023-04-18",
-	"title": "Ghosted",
-	"video": false,
-	"vote_average": 7.3,
-	"vote_count": 513
-}
-]
+    adult: false,
+    backdrop_path: '/22z44LPkMyf5nyyXvv8qQLsbom.jpg',
+    genre_ids: [27, 9648, 53],
+    id: 631842,
+    original_language: 'en',
+    original_title: 'Knock at the Cabin',
+    overview:
+      'While vacationing at a remote cabin, a young girl and her two fathers are taken hostage by four armed strangers who demand that the family make an unthinkable choice to avert the apocalypse. With limited access to the outside world, the family must decide what they believe before all is lost.',
+    popularity: 3012.719,
+    poster_path: '/dm06L9pxDOL9jNSK4Cb6y139rrG.jpg',
+    release_date: '2023-02-01',
+    title: 'Knock at the Cabin',
+    video: false,
+    vote_average: 6.5,
+    vote_count: 907
+  },
+  {
+    adult: false,
+    backdrop_path: '/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg',
+    genre_ids: [28, 12, 878],
+    id: 505642,
+    original_language: 'en',
+    original_title: 'Black Panther: Wakanda Forever',
+    overview:
+      'Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death.  As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.',
+    popularity: 2325.121,
+    poster_path: '/sv1xJUazXeYqALzczSZ3O6nkH75.jpg',
+    release_date: '2022-11-09',
+    title: 'Black Panther: Wakanda Forever',
+    video: false,
+    vote_average: 7.3,
+    vote_count: 3945
+  },
+  {
+    adult: false,
+    backdrop_path: '/nDxJJyA5giRhXx96q1sWbOUjMBI.jpg',
+    genre_ids: [28, 35, 14, 12],
+    id: 594767,
+    original_language: 'en',
+    original_title: 'Shazam! Fury of the Gods',
+    overview:
+      'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.',
+    popularity: 5286.148,
+    poster_path: '/2VK4d3mqqTc7LVZLnLPeRiPaJ71.jpg',
+    release_date: '2023-03-15',
+    title: 'Shazam! Fury of the Gods',
+    video: false,
+    vote_average: 6.8,
+    vote_count: 1380
+  },
+  {
+    adult: false,
+    backdrop_path: '/bT3IpP7OopgiVuy6HCPOWLuaFAd.jpg',
+    genre_ids: [35, 9648, 28],
+    id: 638974,
+    original_language: 'en',
+    original_title: 'Murder Mystery 2',
+    overview:
+      'After starting their own detective agency, Nick and Audrey Spitz land a career-making case when their billionaire pal is kidnapped from his wedding.',
+    popularity: 2099.981,
+    poster_path: '/s1VzVhXlqsevi8zeCMG9A16nEUf.jpg',
+    release_date: '2023-03-28',
+    title: 'Murder Mystery 2',
+    video: false,
+    vote_average: 6.6,
+    vote_count: 920
+  },
+  {
+    adult: false,
+    backdrop_path: '/44immBwzhDVyjn87b3x3l9mlhAD.jpg',
+    genre_ids: [27, 9648, 53],
+    id: 934433,
+    original_language: 'en',
+    original_title: 'Scream VI',
+    overview:
+      'Following the latest Ghostface killings, the four survivors leave Woodsboro behind and start a fresh chapter.',
+    popularity: 3854.657,
+    poster_path: '/wDWwtvkRRlgTiUr6TyLSMX8FCuZ.jpg',
+    release_date: '2023-03-08',
+    title: 'Scream VI',
+    video: false,
+    vote_average: 7.4,
+    vote_count: 908
+  },
+  {
+    adult: false,
+    backdrop_path: '/b9UCfDzwiWw7mIFsIQR9ZJUeh7q.jpg',
+    genre_ids: [10749, 28, 35],
+    id: 868759,
+    original_language: 'en',
+    original_title: 'Ghosted',
+    overview:
+      'Salt-of-the-earth Cole falls head over heels for enigmatic Sadie — but then makes the shocking discovery that she’s a secret agent. Before they can decide on a second date, Cole and Sadie are swept away on an international adventure to save the world.',
+    popularity: 4667.034,
+    poster_path: '/liLN69YgoovHVgmlHJ876PKi5Yi.jpg',
+    release_date: '2023-04-18',
+    title: 'Ghosted',
+    video: false,
+    vote_average: 7.3,
+    vote_count: 513
+  }
+];
 
 export const FRIEND_LIST = [
-	{
-		id: 1,
-		name: 'Tim'
-	},
-	{
-		id: 2,
-		name: 'Jennifer'
-	}
-]
+  {
+    id: 1,
+    name: 'Tim'
+  },
+  {
+    id: 2,
+    name: 'Jennifer'
+  }
+];
 
 export const GENRES = [
-	{
-		"id": 1,
-		"name": "Most Popular"
-	},
-	{
-		"id": 28,
-		"name": "Action"
-	},
-	{
-			"id": 12,
-			"name": "Adventure"
-	},
-	{
-			"id": 16,
-			"name": "Animation"
-	},
-	{
-			"id": 35,
-			"name": "Comedy"
-	},
-	{
-			"id": 80,
-			"name": "Crime"
-	},
-	{
-			"id": 99,
-			"name": "Documentary"
-	},
-	{
-			"id": 18,
-			"name": "Drama"
-	},
-	{
-			"id": 10751,
-			"name": "Family"
-	},
-	{
-			"id": 14,
-			"name": "Fantasy"
-	},
-	{
-			"id": 36,
-			"name": "History"
-	},
-	{
-			"id": 27,
-			"name": "Horror"
-	},
-	{
-			"id": 10402,
-			"name": "Music"
-	},
-	{
-			"id": 9648,
-			"name": "Mystery"
-	},
-	{
-			"id": 10749,
-			"name": "Romance"
-	},
-	{
-			"id": 878,
-			"name": "Science Fiction"
-	},
-	{
-			"id": 10770,
-			"name": "TV Movie"
-	},
-	{
-			"id": 53,
-			"name": "Thriller"
-	},
-	{
-			"id": 10752,
-			"name": "War"
-	},
-	{
-			"id": 37,
-			"name": "Western"
-	}
-]
+  {
+    id: 1,
+    name: 'Most Popular'
+  },
+  {
+    id: 28,
+    name: 'Action'
+  },
+  {
+    id: 12,
+    name: 'Adventure'
+  },
+  {
+    id: 16,
+    name: 'Animation'
+  },
+  {
+    id: 35,
+    name: 'Comedy'
+  },
+  {
+    id: 80,
+    name: 'Crime'
+  },
+  {
+    id: 99,
+    name: 'Documentary'
+  },
+  {
+    id: 18,
+    name: 'Drama'
+  },
+  {
+    id: 10751,
+    name: 'Family'
+  },
+  {
+    id: 14,
+    name: 'Fantasy'
+  },
+  {
+    id: 36,
+    name: 'History'
+  },
+  {
+    id: 27,
+    name: 'Horror'
+  },
+  {
+    id: 10402,
+    name: 'Music'
+  },
+  {
+    id: 9648,
+    name: 'Mystery'
+  },
+  {
+    id: 10749,
+    name: 'Romance'
+  },
+  {
+    id: 878,
+    name: 'Science Fiction'
+  },
+  {
+    id: 10770,
+    name: 'TV Movie'
+  },
+  {
+    id: 53,
+    name: 'Thriller'
+  },
+  {
+    id: 10752,
+    name: 'War'
+  },
+  {
+    id: 37,
+    name: 'Western'
+  }
+];
